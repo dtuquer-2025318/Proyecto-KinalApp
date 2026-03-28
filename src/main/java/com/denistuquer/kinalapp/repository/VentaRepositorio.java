2c18ae36b4c5fbc90d4b4f1ee53f0ab18731f076
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-03-27 18:10:40 -0600

    feat: se agrego el repositorio de venta

---
+++ src/main/java/com/denistuquer/kinalapp/repository/VentaRepositorio.java
@@ -0,0 +1,13 @@
+package com.denistuquer.kinalapp.repository;
+
+import com.denistuquer.kinalapp.entity.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repositorio para la entidad Venta.
+ *
+ * Gracias a JpaRepository, ya incluye métodos para guardar, buscar,
+ * actualizar y eliminar registros sin necesidad de implementarlos nanualmente.
+ */
+public interface VentaRepositorio extends JpaRepository<Venta, Integer> {
+}
