cb0867eefd38c2215b62318ae0716be1aa6a1d85
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-03-27 23:02:41 -0600

    refactor: se modifico el repositorio de detalle venta

--- src/main/java/com/denistuquer/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/denistuquer/kinalapp/repository/DetalleVentaRepository.java
@@ -1,4 +1,7 @@
 package com.denistuquer.kinalapp.repository;
 
-public interface DetalleVentaRepository {
+import com.denistuquer.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
 }
