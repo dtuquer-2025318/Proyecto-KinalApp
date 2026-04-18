840c2ced97db5c3ed9121c3ab594e61612f8e8be
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-04-17 22:38:33 -0600

    refactor: se modifico el repositorio de venta

--- src/main/java/com/denistuquer/kinalapp/repository/VentaRepository.java
+++ src/main/java/com/denistuquer/kinalapp/repository/VentaRepository.java
@@ -3,11 +3,8 @@ package com.denistuquer.kinalapp.repository;
 import com.denistuquer.kinalapp.entity.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Repositorio para la entidad Venta.
- *
- * Gracias a JpaRepository, ya incluye métodos para guardar, buscar,
- * actualizar y eliminar registros sin necesidad de implementarlos nanualmente.
- */
+import java.util.List;
+
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
+    List<Venta> findByClienteDPICliente(String dpi);
 }
