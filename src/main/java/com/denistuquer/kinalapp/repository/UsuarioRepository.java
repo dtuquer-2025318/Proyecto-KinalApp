3ad6ceba113f70f863421b0157fc3b8aa76bdbc0
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-04-17 22:37:18 -0600

    refactor: se modifico el repositorio de usuario

--- src/main/java/com/denistuquer/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/denistuquer/kinalapp/repository/UsuarioRepository.java
@@ -4,4 +4,5 @@ import com.denistuquer.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Usuario findByUsernameAndPassword(String username, String password);
 }
