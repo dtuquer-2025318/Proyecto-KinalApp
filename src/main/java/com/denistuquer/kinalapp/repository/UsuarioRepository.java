29b29fa1f32ce8c706045acc0085ebc1b6011d0b
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-05-02 16:00:57 -0600

    feat: se agregar la búsqueda por username en UsuarioRepository

--- src/main/java/com/denistuquer/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/denistuquer/kinalapp/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@ package com.denistuquer.kinalapp.repository;
 import com.denistuquer.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Usuario findByUsernameAndPassword(String username, String password);
+    Optional<Usuario> findByUsername(String username);
 }
